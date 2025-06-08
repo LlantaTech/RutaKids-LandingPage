778b189aa70b8d83f3adaabc907ef49f3ee396a3
Belen Ramos <129527512+BelenramosR@users.noreply.github.com>
2025-06-08 02:48:39 -0500

    feat: add LoadingScreen
    :D 👍

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 
+import LoadingScreen from './components/ui/LoadingScreen';
 import NavBar from './components/ui/NavBar';
 import Footer from './components/ui/Footer';
 import LanguageSwitcher from './components/ui/LanguageSwitcher';
@@ -13,7 +14,6 @@ const ForSchool = React.lazy(() => import('./components/homepage/ForSchool'));
 const AboutUs = React.lazy(() => import('./components/homepage/AboutUs'));
 const AboutTheProduct = React.lazy(() => import('./components/homepage/AboutTheProduct'));
 const Contact = React.lazy(() => import('./components/homepage/Contact'));
-
 
 const LazySection = ({ Component }) => {
     const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
@@ -28,24 +28,34 @@ const LazySection = ({ Component }) => {
     );
 };
 
-const App = () => {
-    return (
-        <main>
-            <NavBar />
-            <Suspense fallback={<div style={{ height: '100vh' }}>Cargando...</div>}>
-                <Hero />
-            </Suspense>
-            <LazySection Component={WhyRutaKids} />
-            <LazySection Component={HowItWorks} />
-            <LazySection Component={ForParents} />
-            <LazySection Component={ForSchool} />
-            <AboutUs />
-            <LazySection Component={AboutTheProduct} />
-            <LazySection Component={Contact} />
-            <Footer />
-            <LanguageSwitcher />
-        </main>
-    );
-};
+export default function App() {
+    const [loadingComplete, setLoadingComplete] = useState(false);
 
-export default App;
+    return (
+        <>
+            {!loadingComplete && (
+                <LoadingScreen onLoadingComplete={() => setLoadingComplete(true)} />
+            )}
+
+            {loadingComplete && (
+                <div>
+                    <NavBar />
+                    <Suspense fallback={<div style={{ height: '100vh' }}>Cargando...</div>}>
+                        <Hero />
+                    </Suspense>
+                    <LazySection Component={WhyRutaKids} />
+                    <LazySection Component={HowItWorks} />
+                    <LazySection Component={ForParents} />
+                    <LazySection Component={ForSchool} />
+                    <AboutUs />
+                    <LazySection Component={AboutTheProduct} />
+                    <LazySection Component={Contact} />
+                    <Footer />
+                    <LanguageSwitcher />
+                </div>
+            )}
+        </>
+    );
+}
+
+
